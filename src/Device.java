9fb39d67f3ec6aa8b7517a4a59a08e6638ec2369
Lukasz Szczupak <szczupak.lukasz@o2.pl>
2020-01-30 13:26:11 +0100

    Magazyn

---
+++ src/Device.java
@@ -0,0 +1,21 @@
+public class Device {
+    String code;
+    String producer;
+    String type;
+    double price;
+
+    Device(String code, String producer, String type, double price) {
+        this(code, producer, type);
+        this.price = price;
+    }
+
+    Device(String code, String producer, String type) {
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+    }
+
+    String getInfo() {
+        return "kod: " + code + " producent:  " + producer + " typ: " + type + " cena: " + price;
+    }
+}
